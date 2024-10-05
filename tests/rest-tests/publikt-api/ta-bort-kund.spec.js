9683b856439b506b58de8ca337a4c85799b14f96
csn8161 <Poonam.Rai.Asker@csn.se>
2024-10-05 16:37:52 +0200

    Ta bort kund: Ska returnera 404 Not Found, om kund med angivet id inte finns

--- tests/rest-tests/publikt-api/ta-bort-kund.spec.js
+++ tests/rest-tests/publikt-api/ta-bort-kund.spec.js
@@ -7,3 +7,11 @@ test('Ska returnera 204 No Content', async ({ request }) => {
   expect(resp.status()).toBe(204);
   expect(resp.statusText()).toBe('No Content');
 });
+
+test('Ska returnera 404 Not Found, om kund med angivet id inte finns', async ({ request }) => {
+  // delete rest call
+  const resp = await request.delete('/digg/users/1');
+  
+  expect(resp.status()).toBe(404);
+  expect(resp.statusText()).toBe('Not Found');
+});
