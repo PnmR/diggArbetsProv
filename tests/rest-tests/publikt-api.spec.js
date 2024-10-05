f775351352ee577cd3a4fd779bee0f77c0812a9a
csn8161 <Poonam.Rai.Asker@csn.se>
2024-10-05 12:24:16 +0200

    Hämta specifik kund: Ska returnera 404 Not Found om inte kunden finns

--- tests/rest-tests/publikt-api.spec.js
+++ tests/rest-tests/publikt-api.spec.js
@@ -30,3 +30,12 @@ test('Ska returnera JSON med den kund vars id överensstämmer med id i path', a
 
   expect(resp.id).toEqual(path);
 });
+
+test('Ska returnera 404 Not Found om inte kunden finns', async ({ request }) => {
+  const path = 50000000000000000000000000000;
+  // get the json representation of the get rest call
+  const resp = await request.get('/digg/users/' + path);
+
+  expect(resp.status()).toEqual(404);
+  expect(resp.statusText()).toEqual("Not Found");
+});
