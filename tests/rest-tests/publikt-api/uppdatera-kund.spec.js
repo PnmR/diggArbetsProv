1f088a3c401f9c2d10a3a3de595b3f896ccf2470
csn8161 <Poonam.Rai.Asker@csn.se>
2024-10-05 15:32:19 +0200

    Uppdatera kund: Samtliga attribut ska gå att uppdatera (ej id)

--- tests/rest-tests/publikt-api/uppdatera-kund.spec.js
+++ tests/rest-tests/publikt-api/uppdatera-kund.spec.js
@@ -12,6 +12,29 @@ test('Ska ta emot JSON med följande attribut: name, address, phone, email', asy
   expect(isJSON(resp)).toBeTruthy();
 });
 
+test('Samtliga attribut ska gå att uppdatera (ej id)', async ({ request }) => {
+  const patchData = {
+    id: '11',
+    name: 'uppdatera samtliga',
+    address: '85356 Sundsvall',
+    phone: '244-897-8765',
+    email: 'mail@mail.com'
+  };
+  // patch rest call
+  const patchResp = await request.patch('/digg/users/123', patchData);
+
+  // get the json representation of the get rest call
+  const updatedKund = await (await request.get('/digg/users/123')).json();
+
+  //check that all the attributes of kund is updated beside id
+  for (const property in patchData) {
+    if (property != 'id')
+      expect(updatedKund[property]).toBe(patchData[property]);
+    else
+      expect(updatedKund[property]).not.toBe(patchData[property]);
+  }
+});
+
 function isJSON(p) {
   try {
     JSON.parse(p)
