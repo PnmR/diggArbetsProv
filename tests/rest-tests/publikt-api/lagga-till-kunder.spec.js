4eeed39963d2d24821a75ec766daba7d2962239c
csn8161 <Poonam.Rai.Asker@csn.se>
2024-10-05 13:19:40 +0200

    Lägga till kunder: Ska returnera JSON med den skapade kunden, inklusive ett id-attribut

--- tests/rest-tests/publikt-api/lagga-till-kunder.spec.js
+++ tests/rest-tests/publikt-api/lagga-till-kunder.spec.js
@@ -29,3 +29,23 @@ test('Ska inte acceptera att klient försöker sätta id via attributlistan', as
   expect(resp.status()).toBe(400);
   expect(resp.statusText()).toMatch('Bad Request');
 });
+
+test('Ska returnera JSON med den skapade kunden, inklusive ett id-attribut', async ({ request }) => {
+  const postData = {
+    name: 'returnera JSON',
+    address: '85356 Sundsvall',
+    phone: '244-897-8765',
+    email: 'mail@mail.com'
+  };
+  // post rest call
+  const resp = await request.post('/digg/users', {
+    data: postData
+  }).then(res => res.json());
+
+  // returnerad JSON har samma värde som skapade kunden
+  for (const property in postData) {
+    console.log(property + ':' + postData[property]);
+    expect(resp[property]).toBe(postData[property]);
+  }
+  expect(resp).toHaveProperty('id');
+});
