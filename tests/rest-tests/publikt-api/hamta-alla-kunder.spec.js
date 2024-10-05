8d0e3aba9a8868c8a02b091f2120775e79ac38df
csn8161 <Poonam.Rai.Asker@csn.se>
2024-10-05 12:31:30 +0200

    file restructure: split tests into multiple test files

--- tests/rest-tests/publikt-api/hamta-alla-kunder.spec.js
+++ tests/rest-tests/publikt-api/hamta-alla-kunder.spec.js
@@ -22,20 +22,3 @@ test('Ska returnera en lista på max 10 kunder, om inte klienten anger size', as
 
   expect(resp.length).toEqual(10);
 });
-
-test('Ska returnera JSON med den kund vars id överensstämmer med id i path', async ({ request }) => {
-  const path = 123;
-  // get the json representation of the get rest call
-  const resp = await request.get('/digg/users/' + path).then(res => res.json());
-
-  expect(resp.id).toEqual(path);
-});
-
-test('Ska returnera 404 Not Found om inte kunden finns', async ({ request }) => {
-  const path = 50000000000000000000000000000;
-  // get the json representation of the get rest call
-  const resp = await request.get('/digg/users/' + path);
-
-  expect(resp.status()).toEqual(404);
-  expect(resp.statusText()).toEqual("Not Found");
-});
---
+++ tests/rest-tests/publikt-api/hamta-specifik-kund.spec.js
@@ -0,0 +1,18 @@
+const { test, expect } = require('@playwright/test');
+
+test('Ska returnera JSON med den kund vars id överensstämmer med id i path', async ({ request }) => {
+  const path = 123;
+  // get the json representation of the get rest call
+  const resp = await request.get('/digg/users/' + path).then(res => res.json());
+
+  expect(resp.id).toEqual(path);
+});
+
+test('Ska returnera 404 Not Found om inte kunden finns', async ({ request }) => {
+  const path = 50000000000000000000000000000;
+  // get the json representation of the get rest call
+  const resp = await request.get('/digg/users/' + path);
+
+  expect(resp.status()).toEqual(404);
+  expect(resp.statusText()).toEqual("Not Found");
+});
