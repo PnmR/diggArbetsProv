a5d890fa53b8743d7745edc8bf31dbbbb8468ec9
csn8161 <Poonam.Rai.Asker@csn.se>
2024-10-05 13:11:35 +0200

    lägga till kunder: Ska inte acceptera att klient försöker sätta id via attributlistan

--- tests/rest-tests/publikt-api/lagga-till-kunder.spec.js
+++ tests/rest-tests/publikt-api/lagga-till-kunder.spec.js
@@ -13,3 +13,19 @@ test('Ska ta emot JSON med följande attribut: name, address, phone, email', asy
 
   expect(resp.ok).toBeTruthy();
 });
+
+test('Ska inte acceptera att klient försöker sätta id via attributlistan', async ({ request }) => {
+  // post rest call
+  const resp = await request.post('/digg/users', {
+    data: {
+      id: 123,
+      name: 'request attributlistan',
+      address: '85356 Sundsvall',
+      phone: '244-897-8765',
+      email: 'mail@mail.com'
+    }
+  });
+
+  expect(resp.status()).toBe(400);
+  expect(resp.statusText()).toMatch('Bad Request');
+});
